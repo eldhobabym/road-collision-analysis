17d1c082a531abcc0b71906d58243ca3a6b3dc2c
eldhobabym <eldho@sayonetech.com>
2023-12-01 12:29:30 +0530

    Update code for reading data from dynamic path Add main method

--- read_data.py
+++ read_data.py
@@ -3,6 +3,8 @@ from sp_session import get_spark_session
 
 from pyspark.sql.functions import col
 
+from utils import get_file_names
+
 # spark = get_spark_session()
 
 
@@ -11,29 +13,23 @@ class ReadData:
     def __init__(self):
         self.spark = get_spark_session()
 
-    def read_collision_record_data(self):
+    def read_collision_record_data(self, path):
         return (
-            self.spark.read.option("delimiter", ",")
-            .option("header", True)
-            .csv(
-                "/home/ubuntu/project/DE/road-collision-analysis/data/20160924/20160924_CollisionRecords.txt"
-            )
+            self.spark.read.option("delimiter", ",").option("header", True).csv(path)
         ).repartitionByRange("CASE_ID")
 
-    def read_party_record_data(self):
+    def read_party_record_data(self, path):
         return (
             self.spark.read.option("delimiter", ",")
             .option("header", True)
-            .csv(
-                "/home/ubuntu/project/DE/road-collision-analysis/data/20160924/20160924_PartyRecords.txt"
-            ).repartitionByRange("CASE_ID")
+            .csv(path)
+            .repartitionByRange("CASE_ID")
         )
 
-    def read_victim_record_data(self):
+    def read_victim_record_data(self, path):
         return (
             self.spark.read.option("delimiter", ",")
             .option("header", True)
-            .csv(
-                "/home/ubuntu/project/DE/road-collision-analysis/data/20160924/20160924_VictimRecords.txt"
-            ).repartitionByRange("CASE_ID")
+            .csv(path)
+            .repartitionByRange("CASE_ID")
         )
