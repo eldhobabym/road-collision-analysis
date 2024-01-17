73cff5b578bcf504d55d6cf954ced9711345aa03
eldhobabym <eldho@sayonetech.com>
2024-01-17 16:49:36 +0530

    Update main

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 
 from ingest_bronze import IngestData
+
 # from silver_layer import ProcessingSilverLayer
 from silver_layer import ProcessingSilverLayer
 
+
 def main():
-    # IngestData()
+    IngestData()
     ProcessingSilverLayer()
 
- 
 
 if __name__ == "__main__":
     # Execute the main function only if this script is run directly, not if it's imported as a module
